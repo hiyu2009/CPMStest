daf016397a894ed240c86bb3281c9b4997a7c5a9
seokyoon130 <seokyoon130@gmail.com>
2016-07-01 13:32:50 +0900

    Add partner route

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,10 +1,12 @@
 import { provideRouter, RouterConfig } from '@angular/router';
 import { Login } from './pages/login/login.component';
 import { Home } from './pages/home/home.component';
+import { PartnerAdd } from './pages/partner/partnerAdd/partnerAdd.component';
 
 export const routes: RouterConfig = [
   { path: 'login', component: Login },
-  { path: '', component: Home }
+  { path: '', component: Home },
+  { path: 'partner/insert', component: PartnerAdd }
 ];
 export const APP_ROUTER_PROVIDERS = [
   provideRouter(routes)
