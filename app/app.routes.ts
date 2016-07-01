78b812ed1a3624b0626f3dbf804a5a9c7087cb11
hiyu2009 <hiyu2009@naver.com>
2016-07-01 15:43:33 +0900

    route: project/selectList Add

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -2,11 +2,13 @@ import { provideRouter, RouterConfig } from '@angular/router';
 import { Login } from './pages/login/login.component';
 import { Home } from './pages/home/home.component';
 import { PartnerAdd } from './pages/partner/partnerAdd/partnerAdd.component';
+import { ProjectList } from './pages/project/projectList/projectList.component';
 
 export const routes: RouterConfig = [
   { path: 'login', component: Login },
   { path: '', component: Home },
-  { path: 'partner/insert', component: PartnerAdd }
+  { path: 'partner/insert', component: PartnerAdd },
+  { path: 'project/selectList', component: ProjectList }
 ];
 export const APP_ROUTER_PROVIDERS = [
   provideRouter(routes)
