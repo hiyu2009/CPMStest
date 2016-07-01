7384bea16eb34896522a467b5944641abbc9c14a
seokyoon130 <seokyoon130@gmail.com>
2016-07-01 13:41:55 +0900

    Add partner model

---
+++ app/common/models/partner.model.ts
@@ -0,0 +1,11 @@
+import { DeptCodeModel } from './deptCode.model';
+import { PartnerCodeModel } from './partnerCode.model';
+
+export class PartnerModel{
+  public partnerId: number;
+  public partnerName: string;
+  public partnerCode: string;
+  public businessNumber: number;
+  public businessCode : string;
+  public createDate : number;
+}
